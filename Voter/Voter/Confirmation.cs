1dccc0e720a1253c7ba84ccdcda7fd821e2dfc25
unknown <mskwarek@stud.elka.pw.edu.pl>
2014-12-29 11:57:06 +0100

    voter - confirmy

---
+++ Voter/Voter/Confirmation.cs
@@ -0,0 +1,42 @@
+﻿using Org.BouncyCastle.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Voter
+{
+    class Confirmation
+    {
+        private int numColumn;
+        private BigInteger column;
+        private BigInteger token;
+        private BigInteger signedColumn;
+
+
+        public Confirmation(int number)
+        {
+            this.numColumn = number;
+        }
+
+        public BigInteger Column
+        {
+            set { column = value; }
+        }
+
+        public BigInteger Token
+        {
+            set { token = value; }
+        }
+
+        public BigInteger SignedColumn
+        {
+            set { signedColumn = value; }
+        }
+
+        public int Index
+        {
+            get { return numColumn; }
+        }
+    }
+}
